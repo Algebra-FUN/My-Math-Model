b48cacf82e24951049cf772b73790eaceebbcab9
Algebra-FUN <worldcreator@foxmail.com>
2020-07-01 12:25:03 +0800

    update with demo

--- 16-mcm-b/NetFlow.py
+++ 16-mcm-b/NetFlow.py
@@ -3,65 +3,70 @@ import numpy as np
 
 inf = float('inf')
 
-m = 4
 
-# 参数
-OD = np.array([
-    [0,0,0,1000],
-    [0,0,0,0],
-    [0,0,0,0],  
-    [0,0,0,0]    
-])
-T0 = np.array([
-    [inf,2,1,1.5],
-    [inf,inf,inf,2],
-    [inf,inf,inf,1],
-    [inf,inf,inf,inf]
-])
-C = np.array([
-    [1,500,100,100],
-    [1,1,1,500],
-    [1,1,1,100],
-    [1,1,1,1]
-])
-alpha, beta = 1, 2
+def netflow(OD, T0, C, n=30, alpha=1, beta=2):
+    m = len(T0)
 
-# 初始化
-T = np.array(T0)
-Q = np.zeros_like(T0)
+    # 初始化
+    T = np.array(T0)
+    Q = np.zeros_like(T0)
+
+    def updateQ(COD, P):
+        P = np.array(P)
+        for i in range(m):
+            for j in range(m):
+                path = P[i, j]
+                for k in range(len(path)-1):
+                    s, t = path[k], path[k+1]
+                    Q[s, t] += COD[i, j]
+
+    def updateT():
+        return T0*(1+alpha*((Q/C)**beta))
+
+    for i in range(n):
+        _, P = Floyd(T)
+        updateQ(OD/n, P)
+        T = updateT()
+
+    return Q, T
 
 
-def updateQ(COD, P):
-    P = np.array(P)
-    for i in range(m):
-        for j in range(m):
-            path = P[i, j]
-            for k in range(len(path)-1):
-                s, t = path[k], path[k+1]
-                Q[s, t] += COD[i, j]
+if __name__ == "__main__":
+    # 参数设定
+    T0 = np.array([
+        [inf, 2, 1, inf],
+        [inf, inf, inf, 2],
+        [inf, inf, inf, 1],
+        [inf, inf, inf, inf]
+    ])
+    OD = np.array([
+        [0, 0, 0, 1000],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ])
+    C = np.array([
+        [1, 500, 100, 100],
+        [1, 1, 1, 500],
+        [1, 1, 1, 100],
+        [1, 1, 1, 1]
+    ])
 
-def updateT():
-    return T0*(1+alpha*((Q/C)**beta))
+    def evalue(T):
+        sum,i=0,0
+        for v in T.flatten():
+            if v < inf:
+                sum +=v
+                i += 1
+        return sum/i
 
-# 分组批次
-n = 20
+    print('增加道路1-4之前:')
+    Q, T = netflow(OD,T0,C)
+    print('Q=', Q)
+    print('evalue=',evalue(T))
 
-for i in range(n):
-    # print(f'turns {i}/{n}')
-    _, P = Floyd(T)
-    updateQ(OD/n, P)
-    # print(Q)
-    T = updateT()
-    # print(T)
-
-def evalue():
-    value = 0
-    i = 0
-    for v in T.flatten():
-        if v < inf:
-            value += v
-            i += 1
-    return value/i
-
-print(Q)
-print(evalue())
+    print('增加道路1-4之后:')
+    T0[0,3] = 1.5
+    Q, T = netflow(OD,T0,C)
+    print('Q=', Q)
+    print('evalue=',evalue(T))
