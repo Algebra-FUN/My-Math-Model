665a040eaead104cc94c92f7ff5d802061945c01
Algebra-FUN <worldcreator@foxmail.com>
2020-07-01 12:13:30 +0800

    update

--- 16-mcm-b/NetFlow.py
+++ 16-mcm-b/NetFlow.py
@@ -1,24 +1,38 @@
-from .ShortPath import Floyd
+from ShortPath import Floyd
 import numpy as np
 
-m = 5
+inf = float('inf')
+
+m = 4
 
 # 参数
-OD = np.matrix()
-T0 = np.matrix()
-C = np.matrix()
-alpha, beta = 1, 4
+OD = np.array([
+    [0,0,0,1000],
+    [0,0,0,0],
+    [0,0,0,0],  
+    [0,0,0,0]    
+])
+T0 = np.array([
+    [inf,2,1,1.5],
+    [inf,inf,inf,2],
+    [inf,inf,inf,1],
+    [inf,inf,inf,inf]
+])
+C = np.array([
+    [1,500,100,100],
+    [1,1,1,500],
+    [1,1,1,100],
+    [1,1,1,1]
+])
+alpha, beta = 1, 2
 
 # 初始化
-T = np.copy(T0)
-Q = np.zeros(T0)
-
-
-def updateT():
-    T = T0*(1+alpha*((Q/C)**beta))
+T = np.array(T0)
+Q = np.zeros_like(T0)
 
 
 def updateQ(COD, P):
+    P = np.array(P)
     for i in range(m):
         for j in range(m):
             path = P[i, j]
@@ -26,13 +40,28 @@ def updateQ(COD, P):
                 s, t = path[k], path[k+1]
                 Q[s, t] += COD[i, j]
 
+def updateT():
+    return T0*(1+alpha*((Q/C)**beta))
 
 # 分组批次
-n = 10
+n = 20
 
 for i in range(n):
+    # print(f'turns {i}/{n}')
     _, P = Floyd(T)
     updateQ(OD/n, P)
-    updateT()
+    # print(Q)
+    T = updateT()
+    # print(T)
+
+def evalue():
+    value = 0
+    i = 0
+    for v in T.flatten():
+        if v < inf:
+            value += v
+            i += 1
+    return value/i
 
 print(Q)
+print(evalue())
