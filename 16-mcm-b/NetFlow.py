acbbca214ebe892069addf34762404a4c4bd0ca0
Algebra-FUN <worldcreator@foxmail.com>
2020-07-01 11:04:29 +0800

    update

--- 16-mcm-b/NetFlow.py
+++ 16-mcm-b/NetFlow.py
@@ -4,32 +4,35 @@ import numpy as np
 m = 5
 
 # 参数
-odm = np.matrix()
+OD = np.matrix()
 T0 = np.matrix()
 C = np.matrix()
-alpha,beta = 1,4
+alpha, beta = 1, 4
 
 # 初始化
 T = np.copy(T0)
 Q = np.zeros(T0)
 
+
 def updateT():
     T = T0*(1+alpha*((Q/C)**beta))
 
-def updateQ(cq,spm):
+
+def updateQ(CQ, P):
     for i in range(m):
         for j in range(m):
-            path = spm[i,j]
+            path = P[i, j]
             for k in range(len(path)-1):
-                s,t = path[k],path[k+1]
-                Q[s,t] += cq[i,j]
+                s, t = path[k], path[k+1]
+                Q[s, t] += CQ[i, j]
 
-# 分组批次    
+
+# 分组批次
 n = 10
 
 for i in range(n):
-    dis,spm = Floyd(T)
-    updateQ(Q/n,spm)
+    _, P = Floyd(T)
+    updateQ(Q/n, P)
     updateT()
 
 print(Q)
